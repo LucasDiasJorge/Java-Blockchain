a5aaf726d0927919622cc62bfcf03bd3f4dec210
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-02-04 17:46:57 -0300

    Checking if chain is valid

--- Service/src/Main.java
+++ Service/src/Main.java
@@ -15,6 +15,33 @@ public class Main {
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
+
+        System.out.println();
+        System.out.printf("Is chain valid ? %s%n", isChainValid());
+
+    }
+
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
     }
 
 }
